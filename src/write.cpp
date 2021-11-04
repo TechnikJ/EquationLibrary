cb9273157f630a1d4db284e93f2144bca8cee10b
TechnikJ <66751154+TechnikJ@users.noreply.github.com>
2021-11-04 23:28:25 +0100

    Add files via upload

---
+++ src/write.cpp
@@ -0,0 +1,7 @@
+#include "write.hpp"
+#include "../vendor/JSON/json.hpp"
+
+void EQ::Write::write(std::string t_json){
+    std::string m_physics_file = "physics.json";
+    
+}
---
+++ src/write.hpp
@@ -0,0 +1,15 @@
+#ifndef __EQ_WRITE_
+#define __EQ_WRITE_
+
+#include<string>
+
+namespace EQ{
+    class Write{
+
+        public:
+            static void write(std::string t_json);
+
+    };
+}
+
+#endif
