9e3b99330d489f73b7d48f0e87b9669ad5630fe3
TechnikJ <jasonrobo27@gmail.com>
2021-11-06 23:53:09 +0100

    read.cpp

--- src/read.cpp
+++ src/read.cpp
@@ -1,1 +1,11 @@
 #include "read.hpp"
+
+using json = nlohmann::basic_json<>;
+
+void EQ::Read::getData(std::string &t_outputString, std::string t_inputFactor){
+
+}
+
+void EQ::Read::searchFile(std::string &t_outputString, std::string t_searchInputJson){
+
+}
