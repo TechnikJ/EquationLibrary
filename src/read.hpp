9ba3fba9cd911465de47982d73a7a2a8aa28e175
TechnikJ <jasonrobo27@gmail.com>
2021-11-06 23:53:16 +0100

    read.hpp

--- src/read.hpp
+++ src/read.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
 #include <string>
+#include "../vendor/JSON/json.hpp"
+#include<fstream>
+#include<iostream>
+
 
 namespace EQ{
     class Read{
         public:
-            static void getWholeFile(std::string &t_outputstring);
+            static void getData(std::string &t_outputString, std::string t_inputFactor);
+            static void searchFile(std::string &t_outputString, std::string t_searchInputJson);
     };
 } 
