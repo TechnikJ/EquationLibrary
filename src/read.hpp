0b33230fbd85ea993f02ec7be234e0993f3df87b
Iason <jasonrobo27@gmail.com>
2021-12-18 14:24:15 +0100

    ChangeFromJSONToDB

--- src/read.hpp
+++ src/read.hpp
@@ -1,17 +1,13 @@
 #pragma once
 
 #include <string>
-#include "../vendor/JSON/json.hpp"
 #include<fstream>
 #include<iostream>
 
 namespace EQ{
     class Read{
         public:
-            static void getData(std::string &t_outputString, std::string t_inputFactor);
-            static void searchFile(std::string &t_outputString, std::string t_searchInputJson);
-        private:
-            static void splitString(std::string t_inputString, std::vector<std::string> &t_outputVector, char t_splitArg);
+           
 
     };
 } 
--- src/write.hpp
+++ src/write.hpp
@@ -1,17 +1,11 @@
 #pragma once
 
 #include<string>
-#include "../vendor/JSON/json.hpp"
 #include<fstream>
 #include<iostream>
 
 namespace EQ{
     class Write{
-
-        public:
-            static void createNewEntry(std::string t_category, std::string t_json);
-            static void createNewCategory(std::string t_parentCategory,std::string t_categoryName);
-        private:
-            static void splitString(std::string t_inputString, std::vector<std::string> &t_outputVector, char t_splitArg);
+        
     };
 }
