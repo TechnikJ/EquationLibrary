14a23ef30e2aefa087c4c9fb42a755b024b79f4f
TechnikJ <jasonrobo27@gmail.com>
2021-12-18 02:00:25 +0100

    Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
+#include<string>
 #include "src/equationLib.hpp"
 
 int main(){
     EQ::Write::createNewCategory("equations.physic","electricity");
     EQ::Write::createNewEntry("equations.physic.electricity","{\"name\":\"Ohm's Law\",\"equation-latex\":\"\\\\R=\\\\frac{U}{I}=\\\\frac{U^2}{P}=\\\\frac{P}{I^2}\",\"author\":\"Georg Simon Ohm\",\"result\":\"\\\\R\",\"description\":\"The resistance R [Ohm] equals the voltage U [Volt] divided by the current I [Ampere]\",\"sort-word\":[\"resistance\"]}");
+    std::string a;
+    //EQ::Read::getData(a,"equations.physic.optic.0");
     return 0;
 }
