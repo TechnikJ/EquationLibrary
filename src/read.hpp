c22fe174b02a7eee645514af0aadfa51ba01aaeb
TechnikJ <jasonrobo27@gmail.com>
2021-12-18 02:01:34 +0100

    read.hpp

--- src/read.hpp
+++ src/read.hpp
@@ -5,11 +5,13 @@
 #include<fstream>
 #include<iostream>
 
-
 namespace EQ{
     class Read{
         public:
             static void getData(std::string &t_outputString, std::string t_inputFactor);
             static void searchFile(std::string &t_outputString, std::string t_searchInputJson);
+        private:
+            static void splitString(std::string t_inputString, std::vector<std::string> &t_outputVector, char t_splitArg);
+
     };
 } 
